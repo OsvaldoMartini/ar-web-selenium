8d7d2736839d84e016530c4fc972e61c18a147af
Osvaldo Martini <osvaldo.martini@gmail.com>
2024-11-29 01:49:53 +0100

    Update ABRInfoPane.java

--- src/main/java/com/allinweb/ch/component/pane/ABRInfoPane.java
+++ src/main/java/com/allinweb/ch/component/pane/ABRInfoPane.java
@@ -29,7 +29,7 @@ public class ABRInfoPane extends ABRPane {
     public void initUIComponents() {
         // Initialize labels with their corresponding text
         applicationNameLabel = new Label("ABR Web Scan v0.7f Beta Test");
-        compileDateLabel = new Label("Build: 02-10-2024");
+        compileDateLabel = new Label("Build: 29-11-2024");
         copyrightLabel = new Label("Copyright Allinweb AG");
         rightsReservedLabel = new Label("All rights reserved");
 
