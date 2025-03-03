c6c243b0daf50c4032d68310e8d3d7529e854f1d
Osvaldo Martini <osvaldo.martini@gmail.com>
2025-03-03 14:15:20 +0000

    Update ARInfoPane.java

--- src/main/java/com/allinweb/ch/component/pane/ARInfoPane.java
+++ src/main/java/com/allinweb/ch/component/pane/ARInfoPane.java
@@ -28,8 +28,8 @@ public class ARInfoPane extends ARPane {
     @Override
     public void initUIComponents() {
         // Initialize labels with their corresponding text
-        applicationNameLabel = new Label("ARS Web v3.0f Beta Test");
-        compileDateLabel = new Label("Build: 28-02-2025");
+        applicationNameLabel = new Label("ARS Web v3.1f Beta Test");
+        compileDateLabel = new Label("Build: 03-03-2025");
         copyrightLabel = new Label("Copyright Allinweb AG");
         rightsReservedLabel = new Label("All rights reserved");
 
