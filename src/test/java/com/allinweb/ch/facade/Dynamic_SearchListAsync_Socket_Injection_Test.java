6741c8c0317b2654ef38d3475559dea6ce007751
Osvaldo Martini <osvaldo.martini@gmail.com>
2026-04-24 02:38:59 +0200

    Update Dynamic_SearchListAsync_Socket_Injection_Test.java

--- src/test/java/com/allinweb/ch/facade/Dynamic_SearchListAsync_Socket_Injection_Test.java
+++ src/test/java/com/allinweb/ch/facade/Dynamic_SearchListAsync_Socket_Injection_Test.java
@@ -41,9 +41,9 @@ import org.openqa.selenium.WebDriver;
 public class Dynamic_SearchListAsync_Socket_Injection_Test {
 
     private static final String EDGE_DRIVER =
-            "C:\\ARWeb-Martini\\ARWeb-Scanner\\edgedriver-versions\\msedgedriver_64-(147.0.3912.60).exe";
+            "D:\\Projects\\ARWeb-Martini\\ARWeb-Scanner\\edgedriver-versions\\msedgedriver_64-(147.0.3912.60).exe";
 
-    private static final String PLUGINS_DIR = "C:\\ARWeb-Martini\\ARWeb\\plugins";
+    private static final String PLUGINS_DIR = "D:\\Projects\\ARWeb-Martini\\ARWeb\\plugins";
 
     //    private static final String TEST_PAGE = "https://www.inlinea.ch/auth/ui/app/auth/flow/web-app/password";
     //        private static final String TEST_PAGE
